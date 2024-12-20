7d8932a0d8e48f372341599feb26fc865b895e62
Jakob Alfredsson <jakob.o.alfredsson@gmail.com>
2024-12-20 23:42:12 +0100

    Working get-user

--- src/streque/db.clj
+++ src/streque/db.clj
@@ -1,6 +1,6 @@
 (ns streque.db
-  (:require
-   [datomic.client.api :as d]))
+  (:require [datomic.client.api :as d]
+            [clojure.walk]))
 
 ; (require '[datomic.client.api :as d])
 (def client (d/client {:server-type :datomic-local
@@ -11,11 +11,23 @@
 
 (def db (d/db conn))
 
-(def user-schema [{:db/ident :user/user-id
+(def entity-schema [{:db/ident :entity/type
+                     :db/valueType :db.type/keyword
+                     :db/cardinality :db.cardinality/one
+                     :db/doc "Entity type"}])
+
+(def entities [{:entity/type :user}])
+
+(def user-schema [{:db/ident :user/id
                    :db/valueType :db.type/string
                    :db/cardinality :db.cardinality/one
                    :db/unique      :db.unique/identity
                    :db/doc "The id of the user"}
+
+                ;;   {:db/ident :type
+                ;;    :db/valueType :db.type/keyword
+                ;;    :db/cardinality :db.cardinality/one
+                ;;    :db/doc "The type of the entity. Should be :user"}
 
                   {:db/ident :user/first-name
                    :db/valueType :db.type/string
@@ -32,28 +44,44 @@
                    :db/cardinality :db.cardinality/one
                    :db/doc "The display name of the user"}])
 
-(def test-users [{:user/user-id "u1"
+(def test-users [{:user/id "u1"
+                  ;:type :user
                   :user/first-name "Jakob"
                   :user/last-name "Alfredsson"
                   :user/display-name "K-cob"}
-                 {:user/user-id "u2"
+                 {:user/id "u2"
+                  ;:type :user
                   :user/first-name "Test"
                   :user/last-name "Testsson"
                   :user/display-name "Tester"}])
 
-(def all-users-q '[:find (pull ?e [*])
-                   :where [?e :user/user-id _]
-                   [?e ?a ?v]])
+(def user-q '[:find (pull ?user [*])
+              :in $ ?user-id
+              :where [?user :user/id ?user-id]])
 
-(defn get-all-users
-  [db]
-  (-> (d/q all-users-q db)
-      (flatten)))
+(defn get-user
+  "Get the user with the given user-id from the database and trim away :db/id."
+  [db user-id]
+  (-> (d/q user-q db user-id)
+      (ffirst)
+      (dissoc :db/id)))
 
-(defn get-user-by-id
-  [user-id db]
-  (let [all-users (d/q all-users-q db)]
-    (filter (fn [user] (= user-id (:user-id user))) all-users)))
+(def entities-q '[:find (pull ?entity [*])
+                  :where [?entity :entity/type _]])
+
+(defn get-all-entities
+  [db])
+
+;; (def all-entities-q '[:find (pull ?e [*])
+;;                       :where [?e :user/first-name "Jakob"]])
+
+;; (defn get-all-entities
+;;   "Get all entities from the database and trim away :db/id."
+;;   [db]
+;;   (->> (d/q all-entities-q db)
+;;        (flatten)
+;;        (map (fn [user] (dissoc user :db/id)))))
+
 
 (comment
   ; https://docs.datomic.com/client-tutorial/client.html
@@ -61,8 +89,9 @@
   ; https://docs.datomic.com/clojure/index.html#datomic.api/entity
   ; https://stackoverflow.com/questions/14189647/get-all-fields-from-a-datomic-entity
   (d/create-database client {:db-name "streque"})
+  (d/transact conn {:tx-data entity-schema})
   (d/transact conn {:tx-data user-schema})
   (d/transact conn {:tx-data test-users})
-  (d/q all-users-q db)
-  (d/delete-database client {:db-name "streque"})
-  )
+  (get-user db "u1")
+  ; (get-all-entities db)
+  (d/delete-database client {:db-name "streque"}))
