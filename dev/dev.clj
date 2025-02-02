63d0aa258fb5b83b49a1840738a0689696c145e8
Jakob Alfredsson <jakob.o.alfredsson@gmail.com>
2025-02-02 15:20:31 +0100

    This implementation works by writing clojure -X:dev:fullstack but it doesn't seem to work with calva

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,12 +1,22 @@
 (ns dev
   (:require [dev-db]
             [dev-http-server]
-            [streque.edn-api]))
+            [streque.edn-api]
+            [figwheel.main :as figwheel]))
 
-(defn initialize-dev-environment!
+(defn initialize-backend-environment!
   [_]
   (dev-http-server/start-server!)
   (reset! streque.edn-api/db-atom dev-db/db))
 
+(defn initialize-frontend-environment!
+  [_]
+  (figwheel/-main "--build" "dev"))
+
+(defn initialize-fullstack-environment!
+  [_]
+  (initialize-backend-environment! {})
+  (initialize-frontend-environment! {}))
+
 (comment
-  (initialize-dev-environment! {}))
+  (initialize-backend-environment! {}))
